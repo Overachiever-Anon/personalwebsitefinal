a0b028d7513ddf7173b5b5a88d2c03e40f4b38ec
☭/acc Anon <ModernmarxistBoxue@yandex.com>
2025-06-25 18:47:29 +0200

    removed not found

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from '@/components/layout/Header';
+import dynamic from 'next/dynamic';
+
+const Header = dynamic(() => import('@/components/layout/Header'), { ssr: false });
 import Footer from '@/components/layout/Footer';
 
 const geistSans = Geist({
